25509ad1cd130e407611a96f0e60204846e13752
Arturo Paez <A01284988@itesm.mx>
2021-08-31 13:39:23 -0500

    06AreaPrismaRectangular

--- assignments/06AreaPrismaRectangular/src/exercise.py
+++ assignments/06AreaPrismaRectangular/src/exercise.py
@@ -1,11 +1,11 @@
 def area(base,altura):
-    return 
+    return base * altura
 
 def area_prisma(base,altura,profundidad):
     return area(base,altura)*2+area(altura,profundidad)*2+area(base,profundidad)*2
 
 def main():
-    #escribe tu código abajo de esta línea
+    #escribe tu código abajo de esta línea.
     b = float(input("Dame la base: "))
     a = float(input("Dame la altura: "))
     p = float(input("Dame la profundidad: "))
